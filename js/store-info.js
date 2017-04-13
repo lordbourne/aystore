5714fde5d45dc2d1f32e46834d1bc641cd1a7115
lordbourne <l060697802@qq.com>
2017-04-13 17:53:37 +0800

    working on the data filling tech

--- js/store-info.js
+++ js/store-info.js
@@ -1,40 +1,44 @@
 $(document).ready(function() {
 
   $.ajax({
-    url: 'js/store1.json',
+    url: 'js/store.json',
     type: 'GET',
     dataType: 'json',
+    data: {},
+    beforeSend: function(XMLHttpRequest) {
+      // 数据正在加载时的特效
+    },
     success: function (data) {
       // banner 部分, 添加店的相关图片
+      console.log(data);
       var picItems = data.strPicture;
-      var html = '<section class="store-pic" id="banner">';
+      var html = '';
       var len = picItems.length;
-      for (var i=0 i<len; i++) {
+      for (var i=0; i<len; i++) {
         html = [
           html,
           '<img src="',
-          picItems[key];
+          picItems[i],
           '" alt="">'
         ].join('');
       }
-      html = html + '</section>';
-      $('#container').append($(html));
-      // 简介部分
-      //
-      // 详细信息
-
+      $('#banner').append($(html));
+      $('#banner').cycle();
+      // 简介部分与服务信息
+      var srvInfo = data.srvInfo;
 
     }
   });
+  //
 
-  jeDate({
-    dateCell: "#dateinfo",
-    format: "YYYY-MM-DD hh:mm:ss",
-    isinitVal: true,
-    // isTime: true, //isClear:false,
-    minDate: "2014-09-19 00:00:00",
-    okfun: function(val) {
-      alert(val);
-    }
-  });
+  // jeDate({
+  //   dateCell: "#dateinfo",
+  //   format: "YYYY-MM-DD hh:mm:ss",
+  //   isinitVal: true,
+  //   // isTime: true, //isClear:false,
+  //   minDate: "2014-09-19 00:00:00",
+  //   okfun: function(val) {
+  //     alert(val);
+  //   }
+  // });
 });
