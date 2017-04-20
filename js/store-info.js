8cfbe399c55fa5b5e85570eb1a07163bfadcd120
lordbourne <l060697802@qq.com>
2017-04-20 18:17:23 +0800

    some changes, at company

--- js/store-info.js
+++ js/store-info.js
@@ -67,20 +67,27 @@ $(document).ready(function() {
       $secIntro.find('.intro').html(data.srvIntro);
       $secIntro.find('.address').html(data.strCity + data.strDistrict + data.strAddress);
 
+      // 初始化 post 数据
+      var postData = {};
+      // postData.custId = getCustId();
       // 服务价格与选择部分
       var $secOrder = $('#container .sec-order');
       var $iconFull = $secOrder.find('.icon-full');
       data.full == 1 ? $iconFull.show() : $iconFull.hide();
       // ----日期时间选择
+      var ordTime = null;// 保存选择的日期
       $secOrder.find('.sec-date img').click(function(event) {
         jeDate({
           dateCell: "#dateinput",
           format: "YYYY-MM-DD hh:mm:ss",
           isinitVal: true,
-          isTime: true, //isClear:false,
+          isTime: true //isClear:false,
+
           // minDate: "2014-09-19 00:00:00",
         })
       });
+
+
       // ----主服务
       var html = '';
       var items = data.aySrvPrice;
@@ -106,11 +113,68 @@ $(document).ready(function() {
                 ].join('');
       }
       $secOrder.find('.sec-aySrvPrice ul').append($(html));
-      // --------绑定事件
+      // 判断选择的日期位于哪个时间段
+      $secOrder.find('.sec-aySrvPrice checkbox1').click(function(event) {
+        $(this).toggleClass('checked');
+      });
 
 
       // ----其他服务
+      var html = '';
+      var items = data.otherSrvPrice;
+      var item = '';
+      for (var i in items) {
+        item = items[i];
+        html = [
+                  html,
+                  '<li class="clearfloat">',
+                    '<div class="icon checkbox" data-></div>',
+                    '<div class="service">',
+                    item.srvName,
+                    '</div>',
+                    '<div class="price">',
+                      '<span class="txt-number">',
+                        item.price,
+                      '</span>',
+                      '&nbsp;',
+                      item.unit,
+                    '</div>',
+                  '</li>'
+                ].join('');
+      }
+      $secOrder.find('.sec-otherSrvPrice ul').append($(html));
+      $secOrder.find('.sec-otherSrvPrice ul checkbox1').each(function(index, el) {
+        $(this).click(function(event) {
+          $(this).toggleClass('checked');
+        });
+      });
 
+      // 点击支付时的动作
+      $('#botbar a').click(function(event) {
+        event.preventDefault();
+        jeDate({
+          choosefun: function(elem, val) {
+          ordTime = val;
+          console.log(ordTime);
+          postData.ordTime = ordTime;
+        }});
+        // 获取表单数据
+        var $aySrvChoose = $secOrder.find('.sec-aySrvPrice checkbox1');
+        if ($aySrvChoose.hasClass('checked')) {
+          postData.aySrvPrice.id = getTimeIntervalId(ordTime, data.aySrvPrice);
+        } else {
+          postData.aySrvPrice = "";
+        }
+        var $otherSrvChoose = secOrder.find('.sec-otherSrvPrice ul checkbox1');
+        $otherSrvChoose.each(function(index, el) {
+          if ($(this).hasClass('checked')) {
+            postData.otherSrvPrice[index].id = data.otherSrvPrice[index].id;
+          } else {
+            postData.otherSrvPrice[index].id = "";
+          }
+        });
+        window.location.href = "pay.html";
+      });
 
 
 
